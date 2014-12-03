26d59e42f0ffb50c879099caf8e4b5dde0bb2980
Sergio Avedillo <sergio101@neo.tamu.edu>
2014-12-03 11:24:21 -0600

    Php

--- Phptest.php
+++ Phptest.php
@@ -3,14 +3,18 @@
  </head>
  <body>
  	<?php
-		$input = fopen("eventInfo.txt", "r") or die("Unable to open file!");
+		//$input = fopen("eventInfo.txt", "r") or die("Unable to open file!");
 		// Output one line until end-of-file
 			//while(!feof($input)) {
-				$eventName =  fgets($input);
-				$eventTime =  fgets($input);
-				$eventLocation =  fgets($input);
-		}
-		fclose($myfile);
+		//		$eventName =  fgets($input);
+		//		$eventTime =  fgets($input);
+		//		$eventLocation =  fgets($input);
+		//}
+		//fclose($myfile);
+		if ($_GET['ip']):
+        $ip = gethostbyname($_GET['ip']);
+        echo($ip);
+    endif;  
 	?>
  </body>
 </html>
